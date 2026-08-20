334b5336753fccec8131e27993d7aa469d66597e
Xiang <zhangx9119@qq.com>
2026-08-21 00:39:10 +0800

    chore: clean generated build files

--- MDK-ARM/oscilloscope/fft.d
+++
@@ -1,4 +0,0 @@
-oscilloscope\fft.o: ..\Core\Src\FFT.c
-oscilloscope\fft.o: ../Core/Inc/FFT.h
-oscilloscope\fft.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-oscilloscope\fft.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
--- MDK-ARM/oscilloscope/font.d
+++
@@ -1,4 +0,0 @@
-oscilloscope\font.o: ..\Core\Src\font.c
-oscilloscope\font.o: ../Core/Inc/font.h
-oscilloscope\font.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-oscilloscope\font.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
